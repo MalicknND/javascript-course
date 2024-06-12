08040b53ca1eb50fcd1e11fe1567db8e54e6bf14
MalicknND <ndiayemalick062@gmail.com>
2024-06-12 16:38:31 +0200

    🎉 challenge 3 Bonus

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -285,15 +285,36 @@ if (age >= 18) {
 // ******************************************************************************
 // Challenge 3
 
-const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// console.log(scoreDolphins);
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log("Koalas win the trophy");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("Both win the trophy");
+// }
+
+// BONUS
+
+const scoreDolphins = (97 + 112 + 80) / 3;
 console.log(scoreDolphins);
-const scoreKoalas = (88 + 91 + 110) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
 console.log(scoreKoalas);
 
-if (scoreDolphins > scoreKoalas) {
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log("Dolphins win the trophy");
-} else if (scoreDolphins < scoreKoalas) {
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
   console.log("Koalas win the trophy");
-} else if (scoreDolphins === scoreKoalas) {
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
   console.log("Both win the trophy");
+} else {
+  console.log("No one win the trophy");
 }
