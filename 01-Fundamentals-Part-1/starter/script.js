031ded92db4223cdf3f54b2eef20c04130b6e083
MalicknND <ndiayemalick062@gmail.com>
2024-06-12 17:04:37 +0200

    🔖 chore: Add switch statement for day of the week in script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -280,7 +280,7 @@ if (age >= 18) {
                     } else {
                         console.log("Someone else should drive...");
                 }
-    */
+ 
 
 // ******************************************************************************
 // Challenge 3
@@ -317,4 +317,47 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log("Both win the trophy");
 } else {
   console.log("No one win the trophy");
+}
+     */
+// ******************************************************************************
+// The switch statement
+
+const day = "wednesday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy week ends");
+    break;
+  default:
+    console.log("Not a valid day ");
+}
+// other way
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy week ends");
+} else {
+  console.log("Not a valid day ");
 }
