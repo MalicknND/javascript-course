1edc51cae293fc9ce5a6345b19f04937718c77e3
MalicknND <ndiayemalick062@gmail.com>
2024-06-12 13:49:42 +0200

    📝 chore: Add logical operators and driving logic to script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -255,3 +255,29 @@ if (age >= 18) {
 */
 // ******************************************************************************
 // Boolean logic
+// Logical operators
+
+const hasDriverLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//   console.log("Amy is able to drive");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; //C
+
+console.log(hasDriverLicense && hasGoodVision && isTired);
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+  console.log("Amy is able to drive");
+} else {
+  console.log("Someone else should drive...");
+}
