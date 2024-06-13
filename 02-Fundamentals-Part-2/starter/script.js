c876e5df9e2f099bcbca2c42ad269d7ec719e06d
MalicknND <ndiayemalick062@gmail.com>
2024-06-13 07:39:43 +0200

    🔥 Part-2 Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,5 @@
 "use strict"; // En JavaScript, l'utilisation de "use strict"; active le mode strict, qui impose une version plus stricte du langage. Cela peut aider à éviter certaines erreurs courantes et à rendre le code plus sécurisé et performant.
-
-let hasDriversLicense = false;
+/*let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriverLicense = true;
@@ -8,3 +7,26 @@ if (hasDriversLicense) console.log("I can drive");
 
 // const interface = "Audio";
 // const private = 244;
+// ******************************************************************************
+
+// FUNCTIONS
+
+function logger() {
+  console.log("I am Malick");
+}
+// calling / running / invoking function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} and ${oranges}.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+// ******************************************************************************
