87d7f533411b2964184aa45a8c3492097ea03d09
MalicknND <ndiayemalick062@gmail.com>
2024-06-13 07:15:07 +0200

    🔇 Part-2 use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+"use strict"; // En JavaScript, l'utilisation de "use strict"; active le mode strict, qui impose une version plus stricte du langage. Cela peut aider à éviter certaines erreurs courantes et à rendre le code plus sécurisé et performant.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio";
+// const private = 244;
