eff29d45069a435599de1212d846204dd0591d97
MalicknND <ndiayemalick062@gmail.com>
2024-06-14 11:26:02 +0200

    🔐 chore: Add introduction to arrays and exercises

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -130,3 +130,44 @@ checkWinner(scoreDolphins, scoreKoalas);
 */
 
 // ******************************************************************************
+// Introduction to Arrays
+
+const friend1 = "Bamba";
+const friend2 = "Amy";
+const friend3 = "Malick";
+
+const friends = ["Bamba", "Amy", "Malick"];
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Malick";
+const malick = [firstName, "NDIAYE", 2037 - 1998, "Student", friends];
+console.log(malick);
+
+// excercice
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+console.log(ages);
