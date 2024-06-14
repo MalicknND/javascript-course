b87d3c8fa727d009706b00706620c9732f2b6bd0
MalicknND <ndiayemalick062@gmail.com>
2024-06-14 16:31:34 +0200

    chore: Add BMI calculation and comparison for Mark and John

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -261,7 +261,7 @@ console.log(
   `${malick.firstName} has ${malick.friends.length} friends, and her best friend is ${malick.friends[1]}`
 );
 
-*/
+
 
 // ******************************************************************************
 
@@ -301,3 +301,41 @@ console.log(malick.age);
 console.log(malick.age);
 // console.log(malick["calcAge"](1998));
 console.log(malick.getSummary());
+*/
+// ******************************************************************************
+
+// Challenge 3
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
+  );
+}
