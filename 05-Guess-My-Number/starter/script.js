fd47a62cdd086c2686128b9e0f9c3bdb08ebc44f
MalicknND <ndiayemalick062@gmail.com>
2024-06-20 23:47:27 +0200

    DOM Manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+/*
+const message = document.querySelector('.message');
+message.textContent = '🎉 Correct Number!';
+console.log(message);
+
+const number = document.querySelector('.number');
+number.textContent = 13;
+
+const score = document.querySelector('.score');
+score.textContent = 10;
+
+const guess = document.querySelector('.guess');
+guess.value = 23;
+*/
