3c3856a9729389f3c062004317790e07cb2312d3
MalicknND <ndiayemalick062@gmail.com>
2024-06-21 20:40:58 +0200

    working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal'); // Select the modal
+const overlay = document.querySelector('.overlay'); // Select the overlay
+const btnCloseModal = document.querySelector('.close-modal'); // Select the close button
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  // Add the hidden class to the modal and overlay
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  // Remove the hidden class from the modal and overlay
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// addEventListener
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
