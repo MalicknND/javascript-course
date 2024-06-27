3c476e76ff8b5d56002f61ef3f052c53a72d9722
MalicknND <ndiayemalick062@gmail.com>
2024-06-27 23:15:11 +0200

    chore: Refactor script.js file and improve code readability

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// The javascript Engine and Runtime
+
+// The javascript Engine is a program that executes javascript code. It is a part of a browser or a javascript runtime environment like Node.js.
+
+// JS ENGINE = program that executes JS code
+
+//
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
