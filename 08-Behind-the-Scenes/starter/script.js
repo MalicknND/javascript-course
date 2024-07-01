2bb168fe501f6b5983d8adad87060ed00e8272ba
MalicknND <ndiayemalick062@gmail.com>
2024-07-01 23:44:59 +0200

    chore: Update script.js file and add missing comments

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -90,7 +90,7 @@ const z = 3;
 console.log(x === window.x); // true
 console.log(y === window.y); // false
 console.log(z === window.z); // false
-*/
+
 // ******************************************************************************
 // The this Keyword in Practice
 
@@ -131,3 +131,54 @@ matilda.calcAge();
 const f = jonas.calcAge;
 // f(); // TypeError Cannot read property 'year' of undefined because f is a regular function call and not a method call so it doesn't have access to the object that is calling it.
 f();
+
+*/
+
+// ******************************************************************************
+
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Matilda';
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+    // solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+jonas.greet();
+jonas.calcAge();
+
+// Arguments keyword
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
