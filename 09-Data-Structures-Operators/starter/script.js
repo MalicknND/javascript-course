83e61d5b7117041711ef15d665ba2ba6e61346f2
MalicknND <ndiayemalick062@gmail.com>
2024-07-04 09:41:34 +0200

    The spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -40,8 +40,60 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
+// ******************************************************************************
+// Spread Operator
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr); // [1, 2, 7, 8, 9]
+
+const newArr = [1, 2, ...arr];
+console.log(newArr); // [1, 2, 7, 8, 9]
+
+console.log(...newArr); // 1 2 7 8 9
+
+// Copy Array
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu); // ['Pizza', 'Pasta', 'Risotto', 'Gnocci']
+
+// Merge Arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu); // ['Pizza', 'Pasta', 'Risotto', 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad']
+
+// Iterables: arrays, strings, maps, sets. NOT objects, multiple arguments separated by commas
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters); // ['J', 'o', 'n', 'a', 's', ' ', 'S.']
+console.log(...str); // J o n a s
+
+// Real-world example
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1?"),
+  // prompt('Ingredient 2?'),
+  // prompt('Ingredient 3?'),
+];
+
+console.log(ingredients); // ['garlic', 'onion', 'spinach']
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Malick' };
+console.log(newRestaurant);
+
+// Copy Object
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name); // Ristorante Roma
+console.log(restaurant.name); // Classico Italiano
+
+// ******************************************************************************
+// Destructuring Objects
+/*
 //
 restaurant.orderDelivery({
   time: '22:30',
@@ -54,8 +106,6 @@ restaurant.orderDelivery({
   address: 'Via del Sole, 21',
   starterIndex: 1,
 });
-
-// Destructuring Objects
 
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
@@ -84,16 +134,17 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c); // 11 23
+*/
 
 // ******************************************************************************
 /*
-// Destructuring Objects
+// Destructuring Arrays
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-// Destructuring Objects
+// Destructuring Arrays
 const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
@@ -130,5 +181,3 @@ console.log(p, q, r); // 8 1 1
 
 */
 // ******************************************************************************
-
-// Destructuring Objects
