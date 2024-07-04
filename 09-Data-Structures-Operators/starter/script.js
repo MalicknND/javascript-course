9e506b25974c72e7375e8e96f617671af561bf7b
MalicknND <ndiayemalick062@gmail.com>
2024-07-04 11:49:31 +0200

    Rest Pattern and  Parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -43,11 +43,57 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+// ******************************************************************************
+// Rest Pattern and Parameters
+/*
+// 1) Destructuring
+// spread, because on right side of =
+const arr = [1, 2, ...[3, 4]];
+
+// rest, because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others); // 1 2 [3, 4, 5]
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood); // Pizza Risotto ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad']
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays); // {thu: {…}, fri: {…}}
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  numbers.forEach(num => (sum += num));
+
+  console.log(sum);
+};
+
+add(2, 3); // 5
+add(5, 3, 7, 2); // 17
+add(8, 2, 5, 3, 2, 1, 4); // 25
+
+const x = [23, 5, 7];
+add(...x); // 35
+
+// call the function with the spread operator
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach'); // mushrooms, ['onion', 'olives', 'spinach']
+restaurant.orderPizza('mushrooms'); // mushrooms, []
+
+*/
 
 // ******************************************************************************
 // Spread Operator
-
+/*
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr); // [1, 2, 7, 8, 9]
@@ -90,7 +136,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name); // Ristorante Roma
 console.log(restaurant.name); // Classico Italiano
-
+*/
 // ******************************************************************************
 // Destructuring Objects
 /*
