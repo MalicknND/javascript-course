baafcb0e007a626ef95ee9151952daf4c4705733
MalicknND <ndiayemalick062@gmail.com>
2024-07-04 23:41:46 +0200

    Map: Fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,40 @@ const restaurant = {
   },
 };
 // ******************************************************************************
+// Map: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal')); // Map(3) {"name" => "Classico Italiano", 1 => "Firenze, Italy", 2 => "Lisbon, Portugal"}
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name')); // Classico Italiano
+console.log(rest.get(true)); // We are open
+console.log(rest.get(1)); // Firenze, Italy
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close'))); // We are closed
+
+console.log(rest.has('categories')); // true
+rest.delete(2);
+console.log(rest); // Map(6) {"name" => "Classico Italiano", 1 => "Firenze, Italy", "categories" => Array(4), "open" => 11, "close" => 23, false => "We are closed"}
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest.size); // 6
+// rest.clear();
+console.log(rest.size); // 0
+
+console.log(rest.get(arr)); // Test
+
+// ******************************************************************************
 // Sets
 /*
 const orderSet = new Set([
