1e2ae761b95d7f03878d83522c2f2bf952a56452
MalicknND <ndiayemalick062@gmail.com>
2024-07-05 21:08:56 +0200

    Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -3,6 +3,7 @@
 // ******************************************************************************
 
 // Default Parameters
+/*
 const bookings = [];
 const createBooking = function (flightNum, numPassengers = 1, price = 199) {
   // ES5
@@ -21,3 +22,69 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 createBooking('LH123'); // numPassengers = 1, price = 199
 createBooking('LH123', 2, 800); // numPassengers = 2, price = 800
 createBooking('LH123', 2); // numPassengers = 2, price = 199
+
+
+// ******************************************************************************
+// How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight); // LH234
+// console.log(jonas); // {name: "Mr. Jonas Schmedtmann", passport: 24739479284}
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas); // {name: "Mr. Jonas Schmedtmann", passport: 52241907307}
+checkIn(flight, jonas); // {name: "Mr. Jonas Schmedtmann", passport: 52241907307} // Checked in
+*/
+
+// ******************************************************************************
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord); // Original string: JavaScript is the best! // Transformed string: JAVASCRIPT is the best! // Transformed by: upperFirstWord
+transformer('JavaScript is the best!', oneWord); // Original string: JavaScript is the best! // Transformed string: javascriptisthebest! // Transformed by: oneWord
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5); // 👋 👋 👋
