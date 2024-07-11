119da2f26365c84ab89dc80631f80a82494704be
MalicknND <ndiayemalick062@gmail.com>
2024-07-11 15:04:13 +0200

    ✅ closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -110,7 +110,6 @@ greet('Hello')('Jonas'); // Hello Jonas
 const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
 greetArrow('Hi')('Jonas'); // Hi Jonas
 
-*/
 
 // ******************************************************************************
 // The call and apply Methods
@@ -164,3 +163,103 @@ book.apply(swiss, flightData); // George Cooper booked a seat on Swiss Air Lines
 console.log(swiss); // {airline: "Swiss Air Lines", iataCode: "LX", bookings: Array(2)}
 
 book.call(swiss, ...flightData); //is the same as the apply method above (spread operator)
+
+
+// ******************************************************************************
+
+// Closures
+
+let passengerCount = 0;
+const secureBooking = function () {
+  return function () {
+    passengerCount++;
+    // console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+// The booker function has access to the passengerCount variable even though it is not in its scope
+booker(); // 1 passengers
+booker(); // 2 passengers
+booker(); // 3 passengers
+
+// console.dir(booker); // prints the booker function name and the closure arguments
+
+// More Closure Examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 7;
+  f = function () {
+    console.log(b * 2); // 14
+  };
+};
+
+g(); // logs 46
+f(); // logs 46
+console.dir(f);
+
+// The f function is a closure that has access to the variables from both g and h.
+//  re-assigning f function
+h(); // logs 14
+f(); // logs 14
+
+console.dir(f);
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000; // This variable will not be used because the boardPassengers function has its own perGroup variable
+boardPassengers(180, 3); // Will start boarding in 3 seconds // We are now boarding all 180 passengers // There are 3 groups, each with 60 passengers
+
+*/
+
+// ******************************************************************************
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+//  Explanation for the function above:
+// 1. The IIFE is executed immediately when the script is loaded.
+// 2. The header variable is assigned to the h1 element.
+// 3. The event listener is added to the body element.
+// 4. When the body is clicked, the callback function is executed.
+// 5. The callback function changes the color of the header to blue.
+// 6. The callback function has access to the header variable because of the closure.
+// 7. The callback function is executed when the body is clicked.
+// 8. The callback function changes the color of the header to blue.
